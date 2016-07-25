1e2639eb0dff03f89e245446bbb5ac2d4043f2fe
Łukasz Dubiel <bambucha14@gmail.com>
2016-07-25 04:02:24 +0200

    Murmur3 from scala rewritten in java
    [info] Benchmark                                     (d)    (w)   Mode  Cnt        Score        Error  Units
    [info] CountMinSketchBenchmark.updateRandomNumbers    16     16  thrpt   10  5160677,096 ± 353406,242  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers    16    256  thrpt   10  5051564,204 ± 614775,651  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers    16   4096  thrpt   10  4077927,596 ± 471112,322  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers    16  65536  thrpt   10  2762806,964 ± 335388,203  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers   128     16  thrpt   10   549624,418 ±  24382,376  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers   128    256  thrpt   10   496671,622 ±  54879,408  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers   128   4096  thrpt   10   253235,271 ±  21384,410  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers   128  65536  thrpt   10   180593,317 ±   9189,173  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers  1024     16  thrpt   10    67087,705 ±   7991,082  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers  1024    256  thrpt   10    55819,859 ±   3689,422  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers  1024   4096  thrpt   10    18955,689 ±    768,839  ops/s
    [info] CountMinSketchBenchmark.updateRandomNumbers  1024  65536  thrpt   10    10322,591 ±    505,292  ops/s

--- akka-remote/src/main/java/akka/remote/artery/compress/CountMinSketch.java
+++ akka-remote/src/main/java/akka/remote/artery/compress/CountMinSketch.java
@@ -5,17 +5,18 @@
 package akka.remote.artery.compress;
 
 import akka.actor.ActorRef;
+import scala.util.hashing.MurmurHash3;
 
 import java.util.Random;
 
 /**
  * INTERNAL API: Count-Min Sketch datastructure.
- * 
+ * <p>
  * Not thread-safe.
- * 
+ * <p>
  * An Improved Data Stream Summary: The Count-Min Sketch and its Applications
  * https://web.archive.org/web/20060907232042/http://www.eecs.harvard.edu/~michaelm/CS222/countmin.pdf
- * 
+ * <p>
  * This implementation is mostly taken and adjusted from the Apache V2 licensed project `stream-lib`, located here:
  * https://github.com/clearspring/stream-lib/blob/master/src/main/java/com/clearspring/analytics/stream/frequency/CountMinSketch.java
  */
@@ -26,15 +27,13 @@ public class CountMinSketch {
   private int depth;
   private int width;
   private long[][] table;
-  private long[] hashA;
   private long size;
   private double eps;
   private double confidence;
 
   private int[] recyclableCMSHashBuckets;
 
-  
-  
+
   public CountMinSketch(int depth, int width, int seed) {
     this.depth = depth;
     this.width = width;
@@ -44,46 +43,14 @@ public class CountMinSketch {
     initTablesWith(depth, width, seed);
   }
 
-  @SuppressWarnings("unused")
-  public CountMinSketch(double epsOfTotalCount, double confidence, int seed) {
-    // 2/w = eps ; w = 2/eps
-    // 1/2^depth <= 1-confidence ; depth >= -log2 (1-confidence)
-    this.eps = epsOfTotalCount;
-    this.confidence = confidence;
-    this.width = (int) Math.ceil(2 / epsOfTotalCount);
-    this.depth = (int) Math.ceil(-Math.log(1 - confidence) / Math.log(2));
-    recyclableCMSHashBuckets = preallocateHashBucketsArray(depth);
-    initTablesWith(depth, width, seed);
-  }
-
-  @SuppressWarnings("unused")
-  public CountMinSketch(int depth, int width, int size, long[] hashA, long[][] table) {
-    this.depth = depth;
-    this.width = width;
-    this.eps = 2.0 / width;
-    this.confidence = 1 - 1 / Math.pow(2, depth);
-    this.hashA = hashA;
-    this.table = table;
-    this.size = size;
-    recyclableCMSHashBuckets = preallocateHashBucketsArray(depth);
-  }
 
   private void initTablesWith(int depth, int width, int seed) {
     this.table = new long[depth][width];
-    this.hashA = new long[depth];
-    Random r = new Random(seed);
-    // We're using a linear hash functions
-    // of the form (a*x+b) mod p.
-    // a,b are chosen independently for each hash function.
-    // However we can set b = 0 as all it does is shift the results
-    // without compromising their uniformity or independence with
-    // the other hashes.
-    for (int i = 0; i < depth; ++i) {
-      hashA[i] = r.nextInt(Integer.MAX_VALUE);
-    }
   }
 
-  /** Referred to as {@code epsilon} in the whitepaper */
+  /**
+   * Referred to as {@code epsilon} in the whitepaper
+   */
   public double relativeError() {
     return eps;
   }
@@ -92,32 +59,6 @@ public class CountMinSketch {
     return confidence;
   }
 
-  private int hash(long item, int i) {
-    long hash = hashA[i] * item;
-    // A super fast way of computing x mod 2^p-1
-    // See http://www.cs.princeton.edu/courses/archive/fall09/cos521/Handouts/universalclasses.pdf
-    // page 149, right after Proposition 7.
-    hash += hash >> 32;
-    hash &= PRIME_MODULUS;
-    // Doing "%" after (int) conversion is ~2x faster than %'ing longs.
-    return ((int) hash) % width;
-  }
-
-  public void addObject(Object item, long count) {
-    if (count < 0) {
-      // Actually for negative increments we'll need to use the median
-      // instead of minimum, and accuracy will suffer somewhat.
-      // Probably makes sense to add an "allow negative increments"
-      // parameter to constructor.
-      throw new IllegalArgumentException("Negative increments not implemented");
-    }
-    MurmurHash.hashBuckets(item, recyclableCMSHashBuckets, width);
-    for (int i = 0; i < depth; ++i) {
-      table[i][recyclableCMSHashBuckets[i]] += count;
-    }
-    size += count;
-  }
-  
   /**
    * Similar to {@code add}, however we reuse the fact that the hask buckets have to be calculated for {@code add}
    * already, and a separate {@code estimateCount} operation would have to calculate them again, so we do it all in one go.
@@ -130,34 +71,22 @@ public class CountMinSketch {
       // parameter to constructor.
       throw new IllegalArgumentException("Negative increments not implemented");
     }
-    MurmurHash.hashBuckets(item, recyclableCMSHashBuckets, width);
+    Murmur3Hash.hashBuckets(item, recyclableCMSHashBuckets, width);
     for (int i = 0; i < depth; ++i) {
       table[i][recyclableCMSHashBuckets[i]] += count;
     }
     size += count;
     return estimateCount(recyclableCMSHashBuckets);
   }
-  
+
   public long size() {
     return size;
-  }
-  
-  /**
-   * The estimate is correct within {@code 'epsilon' * (total item count)},
-   * with probability {@code confidence}.
-   */
-  public long estimateCount(long item) {
-    long res = Long.MAX_VALUE;
-    for (int i = 0; i < depth; ++i) {
-      res = Math.min(res, table[i][hash(item, i)]);
-    }
-    return res;
   }
 
   /**
    * The estimate is correct within {@code 'epsilon' * (total item count)},
    * with probability {@code confidence}.
-   * 
+   *
    * @param buckets the "indexes" of buckets from which we want to calculate the count
    */
   private long estimateCount(int[] buckets) {
@@ -171,8 +100,119 @@ public class CountMinSketch {
 
   private static class Murmur3Hash {
 
+    /**
+     * Force all bits of the hash to avalanche. Used for finalizing the hash.
+     */
+    private static int avalanche(int hash) {
+      int h = hash;
+
+      h ^= h >>> 16;
+      h *= 0x85ebca6b;
+      h ^= h >>> 13;
+      h *= 0xc2b2ae35;
+      h ^= h >>> 16;
+
+      return h;
+    }
+
+    private static int mixLast(int hash, int data) {
+      int k = data;
+
+      k *= 0xcc9e2d51; //c1
+      k = Integer.rotateLeft(k, 15);
+      k *= 0x1b873593; //c2
+
+      return hash ^ k;
+    }
+
+
+    private static int mix(int hash, int data) {
+      int h = mixLast(hash, data);
+      h = Integer.rotateLeft(h, 13);
+      return h * 5 + 0xe6546b64;
+    }
+
+    public static int hash(Object o) {
+      if (o == null) {
+        return 0;
+      }
+      if (o instanceof ActorRef) { // TODO possibly scary optimisation
+        // ActorRef hashcode is the ActorPath#uid, which is a random number assigned at its creation,
+        // thus no hashing happens here - the value is already cached.
+        // TODO it should be thought over if this preciseness (just a random number, and not hashing) is good enough here?
+        return o.hashCode();
+      }
+      if (o instanceof String) {
+        return hash(((String) o).getBytes());
+      }
+      if (o instanceof Long) {
+        return hashLong((Long) o, 0);
+      }
+      if (o instanceof Integer) {
+        return hashLong((Integer) o, 0);
+      }
+      if (o instanceof Double) {
+        return hashLong(Double.doubleToRawLongBits((Double) o), 0);
+      }
+      if (o instanceof Float) {
+        return hashLong(Float.floatToRawIntBits((Float) o), 0);
+      }
+      if (o instanceof byte[]) {
+        return bytesHash((byte[]) o, 0);
+      }
+      return hash(o.toString());
+    }
+
+    private static final long highLongMask = 0xFFFFFFFF00000000L;
+
+    static int hashLong(long value, int seed) {
+      int h = seed;
+      h = mix(h, (int) (value));
+      h = mixLast(h, (int) ((value & highLongMask) >> 32));
+      return avalanche(h ^ 2);
+    }
+
+    public static int bytesHash(byte[] data, int seed) {
+      int len = data.length;
+      int h = seed;
+
+      // Body
+      int i = 0;
+      while (len >= 4) {
+        int k = data[i] & 0xFF;
+        k |= (data[i + 1] & 0xFF) << 8;
+        k |= (data[i + 2] & 0xFF) << 16;
+        k |= (data[i + 3] & 0xFF) << 24;
+
+        h = mix(h, k);
+
+        i += 4;
+        len -= 4;
+      }
+
+      // Tail
+      int k = 0;
+      if (len == 3) k ^= (data[i + 2] & 0xFF) << 16;
+      if (len >= 2) k ^= (data[i + 1] & 0xFF) << 8;
+      if (len >= 1) {
+        k ^= (data[i] & 0xFF);
+        h = mixLast(h, k);
+      }
+
+      // Finalization
+      return avalanche(h ^ data.length);
+    }
+
+    static void hashBuckets(Object item, int[] hashBuckets, int max) {
+      int hash1 = hash(item); // specialized hash for ActorRef and Strings
+      int hash2 = hashLong(hash1, hash1);
+      final int depth = hashBuckets.length;
+      for (int i = 0; i < depth; i++)
+        hashBuckets[i] = Math.abs((hash1 + i * hash2) % max);
+    }
+
   }
-  
+
   /**
    * This is copied from https://github.com/addthis/stream-lib/blob/master/src/main/java/com/clearspring/analytics/hash/MurmurHash.java
    * Which is Apache V2 licensed.
@@ -187,7 +227,7 @@ public class CountMinSketch {
    */
   // TODO replace with Scala's Murmur3, it's much faster
   private static class MurmurHash {
-  
+
     public static int hash(Object o) {
       if (o == null) {
         return 0;
@@ -218,23 +258,23 @@ public class CountMinSketch {
       }
       return hash(o.toString());
     }
-  
+
     public static int hash(byte[] data) {
       return hash(data, data.length, -1);
     }
-  
+
     public static int hash(byte[] data, int seed) {
       return hash(data, data.length, seed);
     }
-  
+
     public static int hash(byte[] data, int length, int seed) {
       int m = 0x5bd1e995;
       int r = 24;
-  
+
       int h = seed ^ length;
-  
+
       int len_4 = length >> 2;
-  
+
       for (int i = 0; i < len_4; i++) {
         int i_4 = i << 2;
         int k = data[i_4 + 3];
@@ -250,11 +290,11 @@ public class CountMinSketch {
         h *= m;
         h ^= k;
       }
-  
+
       // avoid calculating modulo
       int len_m = len_4 << 2;
       int left = length - len_m;
-  
+
       if (left != 0) {
         if (left >= 3) {
           h ^= (int) data[length - 3] << 16;
@@ -265,75 +305,52 @@ public class CountMinSketch {
         if (left >= 1) {
           h ^= (int) data[length - 1];
         }
-  
+
         h *= m;
       }
-  
+
       h ^= h >>> 13;
       h *= m;
       h ^= h >>> 15;
-  
+
       return h;
     }
-  
+
     public static int hashLong(long data) {
       return hashLong(data, 0);
     }
+
     public static int hashLong(long data, int seed) {
       int m = 0x5bd1e995;
       int r = 24;
-  
-     int h = seed;
+
+      int h = seed;
       // int h = seed ^ length;
-  
+
       int k = (int) data * m;
       k ^= k >>> r;
       h ^= k * m;
-  
+
       k = (int) (data >> 32) * m;
       k ^= k >>> r;
       h *= m;
       h ^= k * m;
-  
+
       h ^= h >>> 13;
       h *= m;
       h ^= h >>> 15;
-  
+
       return h;
     }
-  
-    // Murmur is faster than an SHA-based approach and provides as-good collision
-    // resistance.  The combinatorial generation approach described in
-    // http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
-    // does prove to work in actual tests, and is obviously faster
-    // than performing further iterations of murmur.
-//    public static int[] hashBuckets(String key, int hashCount, int max) {
-//      byte[] b;
-//      try {
-//        b = key.getBytes("UTF-16");// TODO Use the Unsafe trick @patriknw used to access the backing array directly -- via Endre
-//      } catch (UnsupportedEncodingException e) {
-//        throw new RuntimeException(e);
-//      }
-//      return hashBuckets(b, hashCount, max);
-//    }
-    
-//    static int[] hashBuckets(byte[] b, int hashCount, int max) {
-//      // TODO we could reuse the arrays
-//      int[] result = preallocateHashBucketsArray(hashCount);
-//      int hash1 = hash(b, b.length, 0);
-//      int hash2 = hash(b, b.length, hash1);
-//      for (int i = 0; i < hashCount; i++) {
-//        result[i] = Math.abs((hash1 + i * hash2) % max);
-//      }
-//      return result;
-//    }
-    
-    /** Mutates passed in {@code hashBuckets} */
+
+    /**
+     * Mutates passed in {@code hashBuckets}
+     */
     static void hashBuckets(Object item, int[] hashBuckets, int max) {
       int hash1 = hash(item); // specialized hash for ActorRef and Strings
       int hash2 = hashLong(hash1, hash1);
       final int depth = hashBuckets.length;
-      for (int i = 0; i < depth; i++) 
+      for (int i = 0; i < depth; i++)
         hashBuckets[i] = Math.abs((hash1 + i * hash2) % max);
     }
   }
